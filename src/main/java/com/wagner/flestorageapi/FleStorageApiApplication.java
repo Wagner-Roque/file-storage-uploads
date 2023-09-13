dea4cf7783e5ae82d5f6e9aa3fabb6aad4c66065
Wagner Roque <roquewagnertadeu@gmail.com>
2023-09-13 11:40:15 -0300

    first commit

---
+++ src/main/java/com/wagner/flestorageapi/FleStorageApiApplication.java
@@ -0,0 +1,13 @@
+package com.wagner.flestorageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleStorageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleStorageApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wagner/flestorageapi/FleStorageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wagner.flestorageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleStorageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
